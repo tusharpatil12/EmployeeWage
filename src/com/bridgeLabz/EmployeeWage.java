008662fa1b4c52377f29df9624b54b5906793b17
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 <tusharpatil22700@gmail.com>
2022-10-12 21:35:34 -0700

    uc2 daily wge added

--- src/com/bridgeLabz/EmployeeWage.java
+++ src/com/bridgeLabz/EmployeeWage.java
@@ -1,18 +1,25 @@
 package com.bridgeLabz;
 
 public class EmployeeWage {
+	
+	
+	static final int WAGE_PER_HOUR = 20;
+	static final int FULL_TIME_HOUR = 8;
 
 	public static void main(String[] args) {
 		
 		System.out.println("welcome to Employee Wage");
 		
-		
+		int dailywage = 0;
 		int empcheck = (int)Math.floor(Math.random()*10)%2;
-		if(empcheck == 1)
+		if(empcheck == 1) {	
 			System.out.println("Employee is Present");
-		else
+			dailywage = WAGE_PER_HOUR * FULL_TIME_HOUR;
+		}
+		else {
 			System.out.println("Employee is Absent");
-		
+		}
+		System.out.println("Daly Wage =>" +dailywage);
 
 	}
 
