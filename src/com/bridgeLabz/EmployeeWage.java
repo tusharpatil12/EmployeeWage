84da1266c3a9343428392e4e6eb703ef58081bda
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 <tusharpatil22700@gmail.com>
2022-10-28 08:41:47 -0700

    welcome

--- src/com/bridgeLabz/EmployeeWage.java
+++ src/com/bridgeLabz/EmployeeWage.java
@@ -9,43 +9,42 @@ public class EmployeeWage {
 	static final int IS_FULL_TIME = 2;
 	static final int IS_PART_TIME = 1;
 
-	public static void computeEmpWage(){
-	int day = 1;
-	int totalworkinghours = 0;
-	int tatolwage = 0;
-	while(day<=WORKING_DAYS_PER_M0NTH && totalworkinghours< TOTAL_WORKING_HOURS) {
-		int dailywage = 0;
-		int empcheck = (int)Math.floor(Math.random()*10)%3;
-		
-		switch (empcheck) {
-		case IS_FULL_TIME:
-			System.out.println("Employee Full Time");
-			dailywage = WAGE_PER_HOUR * FULL_TIME_HOUR;
-			totalworkinghours = totalworkinghours + FULL_TIME_HOUR;
-			break;
-		case IS_PART_TIME:
-			System.out.println("Employee Part Time");
-			dailywage = WAGE_PER_HOUR * PART_TIME_HOUR;
-			totalworkinghours = totalworkinghours + PART_TIME_HOUR;
+	public static int computeEmpWage(String COMPANY_NAME, int WAGE_PER_HOUR, int WORKING_DAYS_PER_MONTH, int TOTAL_WORKING_HOURS){
+		int day = 1;
+		int totalworkinghours = 0;
+		int tatolwage = 0;
+		while(day <= WORKING_DAYS_PER_M0NTH && totalworkinghours <= TOTAL_WORKING_HOURS) {
+			int dailywage = 0;
+			int empcheck = (int) Math.floor(Math.random()*10) % 3;
 
-			break;
-			
-			
-		default:	
-			System.out.println("Employee is Absent");
+			switch (empcheck) {
+			case IS_FULL_TIME:
+				System.out.println("Employee Full Time");
+				dailywage = WAGE_PER_HOUR * FULL_TIME_HOUR;
+				totalworkinghours = totalworkinghours + FULL_TIME_HOUR;
+				break;
+			case IS_PART_TIME:
+				System.out.println("Employee Part Time");
+				dailywage = WAGE_PER_HOUR * PART_TIME_HOUR;
+				totalworkinghours = totalworkinghours + PART_TIME_HOUR;
+				break;	
+			default:	
+				System.out.println("Employee is Absent");
 
+			}
+			tatolwage = tatolwage + dailywage;
+			System.out.println("Daly Wage => "+day+" =>"+dailywage);
+			day++;
 		}
-		tatolwage = tatolwage + dailywage;
-		System.out.println("Daly Wage => " +dailywage);
-		day++;
-		}
-	System.out.println("Total wage=>" +tatolwage);
-	System.out.println("Total working hours=>" +totalworkinghours);
-		
+		System.out.println("Total wage=>" +tatolwage);
+		System.out.println("Total working hours=>" +totalworkinghours);
+		return tatolwage;
+
 	}	
-public static void main(String[] args) {
-	System.out.println("welcome to Employee Wage");
-	computeEmpWage();
-}
+	public static void main(String[] args) {
+		System.out.println("welcome to Employee Wage");
+		computeEmpWage("D-Mart",20, 2, 10);
+		computeEmpWage("NOKIA",20, 2, 10);
+	}
 }
 
